9d179e49c6127ca0aad8fdc8bae2941a7b6e1f20
husebye <antonhusebye@gmail.com>
2022-11-22 20:30:50 +0100

    testa pages

--- script.js
+++ script.js
@@ -1,1 +1,79 @@
-console.log('test');
+//console.log('henlo');
+
+//Användare & lösenord med behörighet
+const allUsers = [
+    {
+        logInName: "janne",
+        logInPassword: "test",
+    },
+    {
+        logInName: "ooga",
+        logInPassword: "booga",
+    }
+];
+
+
+
+const btnLogin = document.querySelector(".btnLogin");
+const btnLogout = document.querySelector(".btnLogout");
+const text = document.querySelector(".text");
+
+//klick på logga ut kallar på funktion
+btnLogout.addEventListener("click", logOut)
+
+//klick på logga in kallar på funktion
+btnLogin.addEventListener("click", logIn)
+
+//login funktion
+function logIn() {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    for(const user of allUsers){
+        if(username == user.logInName && password == user.logInPassword){
+            localStorage.setItem("user", username);
+            setBodyDataLoggedIn();
+            return;
+        }
+    }
+    document.getElementById("error").style.display = "block";
+    document.getElementById("notLoggedIn").style.display = "none";
+}
+
+//funktion för data vid inloggning & utloggning
+function setBodyDataLoggedIn() {
+    document.getElementById("loginForm").style.display = "none";
+    document.getElementById("logoutForm").style.display = "block";
+    document.getElementById("notLoggedIn").style.display = "none";
+    document.getElementById("loggedIn").style.display = "block";
+    document.getElementById("error").style.display = "none";
+    document.getElementById("whoIsLoggedIn").innerText = localStorage.getItem("user");
+}
+
+function setBodyDataLogOut() {
+    document.getElementById("loginForm").style.display = "block";
+    document.getElementById("logoutForm").style.display = "none";
+    document.getElementById("notLoggedIn").style.display = "block";
+    document.getElementById("loggedIn").style.display = "none";
+    document.getElementById("error").style.display = "none";    
+}
+
+//funktion för inloggad check
+function checkLogIn() {
+    if(localStorage.getItem("user")){
+        setBodyDataLoggedIn();
+    }
+}
+
+//spara till localstorage
+const saveToLocalStorage = (username) => {
+    localStorage.setItem("textInput", username)
+}
+
+//funktion för att logga ut
+function logOut(){
+    setBodyDataLogOut();
+}
+
+//checkar inloggning
+window.onload = checkLogIn();
